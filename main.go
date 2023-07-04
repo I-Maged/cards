b0925f56c784e2fd8d64a41addaeac1664514378
I-Maged <magedhanafy@gmail.com>
2023-07-04 19:57:03 +0300

    save to hard drive

--- main.go
+++ main.go
@@ -3,5 +3,13 @@ package main
 func main() {
 	cards := newDeck()
 
-	cards.print()
+	cards.saveToFile("my_cards")
+
+	// hand, remainingCards := deal(cards, 4)
+
+	// hand.print()
+	// remainingCards.print()
+
+	// fmt.Println("------------")
+	// fmt.Print(hand.toString())
 }
